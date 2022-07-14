a0986f1061ba637144c66ef8f0e0b38364624671
Hema <hemasahu019@gmail.com>
2022-07-14 12:59:05 +0530

    [Hema Added UC2 - Employee Wage Calculation code]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,26 +1,38 @@
 ﻿namespace EmployeeWageComputationLP
-{    /*
-      UC1: Check Employee is Present or Absent
-     */
+{    
+     /// <summary>
+     ///  UC2: Calculate Daily Employee Wage
+     /// </summary>
     public class Program
     {
         //Entrypoint for all console applications
         static void Main(string[] args)
         {
-           //Constant Variable
+            //Constant Variables
             const int EMP_PRESENT = 1;
+            const int WAGE_PER_HOUR = 20;
+
+            //Local Variables
+            int empHour = 0; ;
+            int empWage;
 
             //Generating Random numbers using random class 
             Random random = new Random();  //creating instance of random class
-            int IsPresent = random.Next(0, 2);  //using next() to generate random values in range (0,1)
+            int IsPresent = random.Next(0, 2);  //using next() to generate random values 0 & 1
 
-             //Using selection statement to check and display whether employee is present or absent
-             if (IsPresent == EMP_PRESENT)
-                   Console.WriteLine("Employee is Present");
-             else
-                   Console.WriteLine("Employee is Absent");
-             Console.ReadLine();  
-         
+            //Using selection statement to check and display whether employee is present or absent
+            if (IsPresent == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+                empHour = 8; //if Employee is present assiging full day hour = 8
+            }
+            else
+               Console.WriteLine("Employee is Absent"); //not assigning empHour = 0 as by default value is 0
+            
+            empWage = empHour * WAGE_PER_HOUR; //Calculating Emp Wage( = Emp hour * wage per hour)
+            Console.WriteLine("Employee wage is {0}", empWage); //displaying Emp wage
+            Console.ReadLine();
+
         }
     }
 }
