d5cbb222c56ea58b71b99fc35becf7a36ad5349d
Hema <hemasahu019@gmail.com>
2022-07-14 12:28:02 +0530

    [Hema Added UC1:Employee is Present or absent]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,11 +1,26 @@
 ﻿namespace EmployeeWageComputationLP
-{
-    internal class Program
+{    /*
+      UC1: Check Employee is Present or Absent
+     */
+    public class Program
     {
+        //Entrypoint for all console applications
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to EmployeeWageComputation Practice Assignment");
-            Console.ReadLine();
+           //Constant Variable
+            const int EMP_PRESENT = 1;
+
+            //Generating Random numbers using random class 
+            Random random = new Random();  //creating instance of random class
+            int IsPresent = random.Next(0, 2);  //using next() to generate random values in range (0,1)
+
+             //Using selection statement to check and display whether employee is present or absent
+             if (IsPresent == EMP_PRESENT)
+                   Console.WriteLine("Employee is Present");
+             else
+                   Console.WriteLine("Employee is Absent");
+             Console.ReadLine();  
+         
         }
     }
 }
