d34eef5c4bfd5c8e04eaf457ba7563b54ad72f5b
Hema <hemasahu019@gmail.com>
2022-07-14 10:44:15 +0530

    [Hema Added Welcome to Employee Wage Computation code]

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputationLP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to EmployeeWageComputation Practice Assignment");
+            Console.ReadLine();
+        }
+    }
+}
