5c29042c2fcf1b0cf24c830ec66b01b05ea44e04
Hema <hemasahu019@gmail.com>
2022-07-14 13:26:00 +0530

    [Hema Adding Part time emp & wage]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,15 +1,14 @@
 ﻿namespace EmployeeWageComputationLP
-{    
-     /// <summary>
-     ///  UC2: Calculate Daily Employee Wage
-     /// </summary>
+{   /// <summary>
+    ///  UC3: Add Part time Employee & Wage (Assuming Full time hour = 8 and Part time hour = 4)
+    /// </summary>
     public class Program
-    {
-        //Entrypoint for all console applications
+    {   //Entrypoint for all console applications
         static void Main(string[] args)
         {
             //Constant Variables
-            const int EMP_PRESENT = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int WAGE_PER_HOUR = 20;
 
             //Local Variables
@@ -18,13 +17,18 @@
 
             //Generating Random numbers using random class 
             Random random = new Random();  //creating instance of random class
-            int IsPresent = random.Next(0, 2);  //using next() to generate random values 0 & 1
+            int empCheck = random.Next(0, 3);  //using next() method to generate random values 0, 1 & 2
 
-            //Using selection statement to check and display whether employee is present or absent
-            if (IsPresent == EMP_PRESENT)
+            //Using selection statement to check for full time and part time employee
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
-                empHour = 8; //if Employee is present assiging full day hour = 8
+                Console.WriteLine("Full time Employee is Present");
+                empHour = 8; //Full time employee is present assiging full time hour = 8
+            }
+            else if(empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Part time Employee is Present");
+                empHour = 4; //Full time employee is present, assiging part time hour = 4
             }
             else
                Console.WriteLine("Employee is Absent"); //not assigning empHour = 0 as by default value is 0
