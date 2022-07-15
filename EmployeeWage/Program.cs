651f061ea6d6999ba5dc678c59d8df3bdf229e13
Hema <hemasahu019@gmail.com>
2022-07-15 12:17:45 +0530

    [Hema Added code to cal. wages with condition]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeWageComputationLP
 {   /// <summary>
-    ///  UC5 : Calculating Wages for a Month assuming 20 Working days
+    ///  UC6 : Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours
     /// </summary>
     public class Program
     {   //Entrypoint for all console applications
@@ -11,18 +11,21 @@
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int WAGE_PER_HOUR = 20;
-            const int NUM_WORKING_DAYS = 20;
+            const int MAX_NUM_WORKING_DAYS = 20;
+            const int MAX_HOUR = 100;
+               
 
             //Local Variables
             int empHour = 0;
             int empWage = 0;
             int totalWage = 0;
-            int day;
+            int day = 1;
+            int workingHour = 0;
 
             //Generating Random numbers using random class 
             Random random = new Random();  //creating instance of random class
-
-            for (day = 1; day <= NUM_WORKING_DAYS; day++)
+            //using while loop as we are not aware as how many iteration is required to get the desired output
+            while ( day <= MAX_NUM_WORKING_DAYS && workingHour < MAX_HOUR)
             {
                 int empCheck = random.Next(0, 3);  //using next() method to generate random values 0, 1 & 2
 
@@ -30,25 +33,28 @@
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
-                        Console.WriteLine("\nFull time Employee is Present");
-                        empHour = 8; //Full time employee is present assiging full time hour = 8
+                        Console.WriteLine("\nFull time employee is Present");
+                        empHour = 8; //Employee is present full time, assiging full time hour = 8
                         break;
 
                     case IS_PART_TIME:
-                        Console.WriteLine("\nPart time Employee is Present");
-                        empHour = 4; //Part time employee is present, assiging part time hour = 4
+                        Console.WriteLine("\nPart time employee is Present");
+                        empHour = 4; //Employee is present part time, assiging part time hour = 4
                         break;
 
                     default:
-                        Console.WriteLine("\nEmployee is Absent"); //initially initialized empHour=0, no need to re-assign if Emp is absent
+                        Console.WriteLine("\nEmployee is Absent"); 
+                        empHour = 0; //Employee is absent, assiging empHour = 0
                         break;
                 }
-
-                empWage = empHour * WAGE_PER_HOUR; //Calculating Emp Wage( = Emp hour * wage per hour)
+                
+                workingHour += empHour;
+                empWage = empHour * WAGE_PER_HOUR; //Calculating Emp Wage( = Emp hour * wage per hour) for one day
                 Console.WriteLine("Employee wage for day-{0} is {1}", day, empWage); //displaying Emp wage
-                totalWage += empWage;  //total wage for the month
+                totalWage += empWage;  //total wage for max day or max hour
+                day++; //incrementing day after every iteration and to avoid infinite while loop
             }
-            Console.WriteLine("\nTotal Wage for the month ({0} Working days) is : {1} ", day-1, totalWage);
+            Console.WriteLine("\nTotal Wage for {0} day (working hour {1}) is {2}", day-1, workingHour, totalWage);
             Console.ReadLine();
 
         }
